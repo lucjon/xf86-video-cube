cda1e68ec0b14caeb7220711fb5be6c168f668fb
DeltaResero <deltaresero@zoho.com>
2013-11-15 01:00:48 -0400

    Add autogen-clean script and cleanup more

---
+++ autogen-clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -f Makefile ]; then
+	echo "Making make distclean..."
+	make distclean
+fi
+echo "Removing autogenned files..."
+rm -f aclocal.m4 config.guess config.sub configure install-sh missing mkinstalldirs Makefile.in ltmain.sh stamp-h.in */Makefile.in ltconfig stamp-h config.h.in
+rm -f -r autom4te.cache
+echo "Done."
